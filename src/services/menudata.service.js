7b76f9d9741959b019fa20689d94bcd6bb1be5a8
c1324737 <c1324737@interno.bb.com.br>
2022-08-10 17:44:54 -0300

    feat: cria consulta a api

--- src/services/menudata.service.js
+++ src/services/menudata.service.js
@@ -3,20 +3,23 @@
 
   angular
     .module('DataModule')
-    .service('MenuDataService', MenuDataService);
+    .service('MenuDataService', MenuDataService)
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 
-  MenuDataService.$inject = ['$http']
+  MenuDataService.$inject = ['$http', 'ApiBasePath'];
 
-  function MenuDataService($http) {
-    var service = this;
+  function MenuDataService($http, ApiBasePath) {
+    let service = this;
 
-    service.getAllCategories = function() {
-      
+    service.getAllCategories = function ($http, ApiBasePath) {
+      let url = `${ApiBasePath}/categories.json`;
+      return $http.get(url);
     };
 
-    service.getItemsForCategory = function(categoryShortName) {
-
+    service.getItemsForCategory = function (categoryShortName) {
+      let url = `${ApiBasePath}/menu_items.json?category=${categoryShortName}`;
+      return $http.get(url);
     };
   }
 
